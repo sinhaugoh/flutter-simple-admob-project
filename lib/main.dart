d0e3af2215f38cf747288d57cb51e801e08bcfcf
Goh Sin Hau <shgoh025@mymail.sim.edu.sg>
2021-09-05 19:55:01 +0800

    configure google ads

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:simple_admob_app/presentation/home_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
   runApp(AppWidget());
 }
 
